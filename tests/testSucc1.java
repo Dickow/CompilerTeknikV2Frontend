4ad3572d6ccb8928e5cee6bad568b823fd0546a2
Dickow <Dickow@10.16.174.24>
2014-10-28 16:14:49 +0100

    en ny succ test tilføjet

---
+++ tests/testSucc1.java
@@ -0,0 +1,31 @@
+public class testSucc1 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		// Vi opretter en ny instans af A og prøver dens metoder af 
+		A newA = new A(10, "Jeppe");
+
+		newA.printName();
+		newA.printNumber();
+
+		class A {
+			private int number;
+			private String name;
+
+			public A(int number, String name) {
+				this.number = number;
+				this.name = name;
+
+			}
+
+			public void printName() {
+				System.out.println("your name is: " + name);
+			}
+
+			public void printNumber() {
+				System.out.println("Your number is: " + number);
+			}
+		}
+	}
+}
